225648271c20e655219cfe66878678beda6f8531
Joe King <tiptop1112@gmail.com>
2022-07-12 04:32:31 +0000

    Completed listening for submit listener and it's functionality.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+// Listening for the $photoUrl input element in order to change the src attribute of the img element
+
+var $photoUrl = document.querySelector('#photo-url');
+var $img = document.querySelector('img');
+
+$photoUrl.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  var $targetUrl = event.target.value;
+  var $imgSrc = $img.getAttribute('src');
+  if ($imgSrc !== $targetUrl) {
+    $img.setAttribute('src', $targetUrl);
+  }
+}
+
+// Listening for 'submit' events on the #journal-entry form element
+
+var $journalEntry = document.querySelector('#journal-entry');
+// var $title = $journalEntry.elements.title.value;
+// var $photo = $journalEntry.elements.photourl;
+
+function handleSubmit(event) {
+  var newObject = {};
+  event.preventDefault();
+  var $title = $journalEntry.elements.title.value;
+  var $photo = $journalEntry.elements.photourl.value;
+  var $notes = $journalEntry.elements.notes.value;
+  Object.assign(newObject, { title: $title, photo: $photo, notes: $notes });
+  newObject.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(newObject);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $journalEntry.reset();
+}
+
+$journalEntry.addEventListener('submit', handleSubmit);
